d6eb9bbeb44a9b186eb0eb655f9a8332f37070d0
John Chia <john.chia0106@gmail.com>
2022-06-30 02:42:12 -0700

    add jsx and implement carousel actions

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,10 @@
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import { Col, Row } from "react-bootstrap";
+import meter1 from "../assets/img/meter1.svg";
+import meter2 from "../assets/img/meter2.svg";
+import meter3 from "../assets/img/meter3.svg";
+import colorSharp from "../assets/img/color-sharp.png";
 
 export const Skills = () => {
   const responsive = {
@@ -18,6 +23,50 @@ export const Skills = () => {
     mobile: {
       breakpoint: { max: 464, min: 0 },
       items: 1,
-    }
+    },
   };
+
+  return (
+    <section className="skill" id="skills">
+      <container>
+        <Row>
+          <Col>
+            <div className="skill-bx">
+              <h2>Skills</h2>
+              <p>descript</p>
+              <Carousel
+                responsive={responsive}
+                infinite={true}
+                className="skill-slider"
+                autoPlay
+                autoPlaySpeed={4000}
+              >
+                <div className="item">
+                  <img src={meter1} alt="Image1" />
+                  <h5>Web Development</h5>
+                </div>
+                <div className="item">
+                  <img src={meter2} alt="Image2" />
+                  <h5>Web Development</h5>
+                </div>
+                <div className="item">
+                  <img src={meter3} alt="Image3" />
+                  <h5>Web Development</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="Image4" />
+                  <h5>Web Development</h5>
+                </div>
+              </Carousel>
+            </div>
+          </Col>
+        </Row>
+      </container>
+      <img
+        className="backgrond-img-left"
+        src={colorSharp}
+        alt="skills-background-left"
+      />
+    </section>
+  );
 };
