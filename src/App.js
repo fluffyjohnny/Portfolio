c242cc4c62a179d58377ec4c42c6cbfe3694403d
John Chia <john.chia0106@gmail.com>
2022-07-06 02:50:52 -0700

    add footer component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
 import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
+import { Footer } from './components/Footer';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
+      <Footer />
     </div>
   );
 }
