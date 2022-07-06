3bfeeee28db2fe09f9bd1770413a6eac84f40f21
John Chia <john.chia0106@gmail.com>
2022-07-06 02:51:13 -0700

    create footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,38 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { MailchimpForm } from "./MailchimpForm";
+import { Contact } from "./Contact";
+import logo from "../assets/img/logo.svg";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-item-center">
+          <Contact />
+          <MailchimpForm />
+          <Col sm={6}>
+            <img src={logo} alt="Logo" />
+          </Col>
+
+          <Col sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+              <a href="1">
+                <img sec={navIcon1} alt="1" />
+              </a>
+              <a href="2">
+                <img sec={navIcon2} alt="1" />
+              </a>
+              <a href="3">
+                <img sec={navIcon3} alt="1" />
+              </a>
+            </div>
+            <p>CopyRight 2022. All Right Reserved by John Chia</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
