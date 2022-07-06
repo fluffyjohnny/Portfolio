732d87e22bcbb9c0ed0d0bb5a784c3733b962f70
John Chia <john.chia0106@gmail.com>
2022-07-06 02:51:36 -0700

    create newsletter component

---
+++ src/components/Newsletter.js
@@ -0,0 +1,50 @@
+import { Alert, Col, Row } from "react-bootstrap";
+import { useEffect, useState } from "react";
+
+export const Newsletter = ({ onValidated, status, message }) => {
+  const [email, setEmail] = useState("");
+
+  useEffect(() => {
+    if (status === "success") clearFields();
+  }, [status]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    email &&
+      email.indexOf("@") > -1 &&
+      onValidated({
+        EMAIL: email,
+      });
+  };
+
+  const clearFields = () => {
+    setEmail("");
+  };
+
+  return (
+    <Col lg={12}>
+      <div className="newsletter-bx">
+        <Row>
+          <Col lg={12} md={6} xl={5}>
+            {status === "sending" && <Alert>Sending...</Alert>}
+            {status === "error" && <Alert variant="danger">{message}</Alert>}
+            {status === "success" && <Alert variant="success">{message}</Alert>}
+          </Col>
+          <Col md={6} xl={7}>
+            <form onSubmit={handleSubmit}>
+              <div className="new-mail-bx">
+                <input
+                  value={email}
+                  type="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Email"
+                />
+                <button type="submit">Submit</button>
+              </div>
+            </form>
+          </Col>
+        </Row>
+      </div>
+    </Col>
+  );
+};
