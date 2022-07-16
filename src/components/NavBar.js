66454562d2453807721e22a813ebec7bebf9a915
John Chia <john.chia0106@gmail.com>
2022-07-16 02:45:47 -0700

    change nav icon and add github button and link href

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import logo from '../assets/img/logo.svg';
+import logo from '../assets/img/logo.png';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
+import navIcon4 from '../assets/img/nav-icon4.svg';
 
 export const NavBar = () => {
 
@@ -45,9 +46,10 @@ export const NavBar = () => {
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="" /></a>
-              <a href="#"><img src={navIcon2} alt="" /></a>
-              <a href="#"><img src={navIcon3} alt="" /></a>
+              <a href="https://www.linkedin.com/in/john-chia-744b30161/" target="_blank" rel="noreferrer"><img src={navIcon1} alt="LinkedIn" /></a>
+              <a href="https://www.facebook.com/fluffyjohnnny" target="_blank" rel="noreferrer"><img src={navIcon2} alt="Facebook" /></a>
+              <a href="https://www.instagram.com/fluffyjohnny/" target="_blank" rel="noreferrer"><img src={navIcon3} alt="Instagram" /></a>
+              <a href="https://github.com/fluffyjohnny" target="_blank" rel="noreferrer"><img src={navIcon4} alt="Github" /></a>
             </div>
             <button className="vvd" onClick={() => console.log('connect')}><span>Let’s Connect</span></button>
           </span>
