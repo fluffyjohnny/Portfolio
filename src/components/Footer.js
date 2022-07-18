ae8a524ad9edad59107dc75ebf4a149f0f60bfe6
John Chia <john.chia0106@gmail.com>
2022-07-17 22:07:52 -0700

    change and link social icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ import logo from "../assets/img/logo.png";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
+import navIcon4 from "../assets/img/nav-icon4.svg";
 
 export const Footer = () => {
   return (
@@ -19,14 +20,33 @@ export const Footer = () => {
 
           <Col sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="1">
-                <img sec={navIcon1} alt="1" />
+              <a
+                href="https://www.linkedin.com/in/john-chia-744b30161/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={navIcon1} alt="LinkedIn" />
               </a>
-              <a href="2">
-                <img sec={navIcon2} alt="1" />
+              <a
+                href="https://www.facebook.com/fluffyjohnnny"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={navIcon2} alt="Facebook" />
               </a>
-              <a href="3">
-                <img sec={navIcon3} alt="1" />
+              <a
+                href="https://www.instagram.com/fluffyjohnny/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={navIcon3} alt="Instagram" />
+              </a>
+              <a
+                href="https://github.com/fluffyjohnny"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={navIcon4} alt="Github" />
               </a>
             </div>
             <p>CopyRight 2022. All Right Reserved by John Chia</p>
