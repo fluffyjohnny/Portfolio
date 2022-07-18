1235c2038416cca28923480393208efbcee1da84
John Chia <john.chia0106@gmail.com>
2022-07-17 22:08:32 -0700

    add props and active links

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -1,15 +1,17 @@
-import { Col } from "react-bootstrap"
+import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ title, description, imgUrl, projLink }) => {
   return (
     <Col sm={6} md={4}>
       <div className="proj-imgbx">
-        <img src={imgUrl} alt="img-url"/>
+        <img src={imgUrl} alt="img-url" />
         <div className="proj-txtx">
-          <h4>{title}</h4>
+          <a href={projLink} ult={"proj-link"} target="_blank" rel="noreferrer">
+            <h4>{title}</h4>
+          </a>
           <span>{description}</span>
         </div>
       </div>
     </Col>
-  )
-}
+  );
+};
